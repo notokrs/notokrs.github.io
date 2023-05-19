3bb806e413a4e95220c0aaacb9d7df43bf91dcca
notokrs <rusnoto030@gmail.com>
2023-05-19 16:12:44 +0700

    refactor: change set icon theme implementation for desktop and mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,15 +21,13 @@ document.addEventListener("DOMContentLoaded", () => {
       "href",
       base_url + "/assets/img/static/favicon-dark.png"
     );
-    document.getElementById("theme-toggle").innerHTML =
-      '<i class="fa-regular fa-sun"></i>';
+    setIcon("light");
   } else {
     favicon.setAttribute(
       "href",
       base_url + "/assets/img/static/favicon-light.png"
     );
-    document.getElementById("theme-toggle").innerHTML =
-      '<i class="fa-regular fa-moon"></i>';
+    setIcon("dark");
   }
 
   if (theme === null && systemInitiatedDark.matches) {
@@ -37,44 +35,69 @@ document.addEventListener("DOMContentLoaded", () => {
       "href",
       base_url + "/assets/img/static/favicon-dark.png"
     );
-    document.getElementById("theme-toggle").innerHTML =
-      '<i class="fa-regular fa-sun"></i>';
+    setIcon("light");
   } else if (theme === null && systemInitiatedDark.matches === false) {
     favicon.setAttribute(
       "href",
       base_url + "/assets/img/static/favicon-light.png"
     );
-    document.getElementById("theme-toggle").innerHTML =
-      '<i class="fa-regular fa-moon"></i>';
+    setIcon("dark");
   }
 });
 
 function themeToggle() {
   const theme = localStorage.getItem("theme");
+
   if (theme === "dark") {
     document.documentElement.setAttribute("data-theme", "light");
     localStorage.setItem("theme", "light");
-    document.getElementById("theme-toggle").innerHTML =
-      '<i class="fa-regular fa-moon"></i>';
+
+    setIcon("dark");
   } else if (theme === "light") {
     document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark");
-    document.getElementById("theme-toggle").innerHTML =
-      '<i class="fa-regular fa-sun"></i>';
+
+    setIcon("light");
   } else if (systemInitiatedDark.matches) {
     document.documentElement.setAttribute("data-theme", "light");
     localStorage.setItem("theme", "light");
-    document.getElementById("theme-toggle").innerHTML =
-      '<i class="fa-regular fa-moon"></i>';
+
+    setIcon("dark");
   } else {
     document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark");
-    document.getElementById("theme-toggle").innerHTML =
-      '<i class="fa-regular fa-sun"></i>';
+
+    setIcon("light");
   }
 }
 
 function goTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+function menuToggle() {
+  const mobileNav = document.getElementById("nav-mobile");
+  const backdrop = document.getElementById("nav-mobile-backdrop");
+
+  if (mobileNav.classList.contains("show-nav-mobile")) {
+    mobileNav.classList.remove("show-nav-mobile");
+    backdrop.style.display = "none";
+  } else {
+    mobileNav.classList.add("show-nav-mobile");
+    backdrop.style.display = "block";
+  }
+}
+
+function setIcon(type) {
+  const themeIcons = document.getElementsByClassName("theme-toggle");
+  if (type === "dark") {
+    [...themeIcons].forEach(
+      (el) => (el.innerHTML = '<i class="fa-regular fa-moon"></i>')
+    );
+  } else if (type === "light") {
+    [...themeIcons].forEach(
+      (el) => (el.innerHTML = '<i class="fa-regular fa-sun"></i>')
+    );
+  }
 }
